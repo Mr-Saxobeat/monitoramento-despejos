d536c11d54491eb9ff3ff7a5873b0a7b9c2fe5ec
Weiglas <weiglasribeiro@gmail.com>
2021-05-03 19:20:53 -0300

    Adiciona hero image

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container-fluid">
-    <b-img :src="require('@/assets/img/mtst.jpg')" class="image">
-    </b-img>
+    <div class="hero-image">
+      <div class="hero-text row">
+        <div class="col-sm" style="color: #D1520F; font-size: 35px"><strong>DENUNCIE</strong></div>
+        <div class="col-sm" style="color: #111111; font-size: 25px">Mapeamento de despejos  no Espírito Santo</div>
+        <div class="col-sm" style="color: #000000; font-size: 25px">Conheça seus direitos</div>
+      </div>  
+    </div>
   </div>
 </template>
 
@@ -10,7 +15,31 @@ export default {}
 </script>
 
 <style scoped>
-.container-fluid {
+.hero-image {
+  height: 100%;
+  width: 100%;
+  background-image: url('~assets/img/foto-crianca.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  height: 110px;
+  width: 100%;
   margin: 0;
+  padding: 0;
+  text-align: center;
+  position: absolute;
+  color: white;
+  bottom: 0;
+  background-color: rgba(211, 210, 210, 0.6);
+}
+
+.hero-text div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
