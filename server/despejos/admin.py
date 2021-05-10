b49db4f3c23e385b5e80cfc4d23be395f6192c33
Weiglas <weiglasribeiro@gmail.com>
2021-05-10 19:06:10 -0300

    Adiciona classe DespejoAdmin
    - Com o list_display correto

--- server/despejos/admin.py
+++ server/despejos/admin.py
@@ -1,7 +1,11 @@
 from django.contrib.gis import admin
 from despejos.models import Despejo, LayerDefinition, Legenda, Cor
 
-admin.site.register(Despejo, admin.GeoModelAdmin)
+class DespejoAdmin(admin.GeoModelAdmin):
+    list_display = ('nome_ocupacao', 'cidade')
+
+
+admin.site.register(Despejo, DespejoAdmin)
 admin.site.register(LayerDefinition)
 admin.site.register(Legenda)
 admin.site.register(Cor)
