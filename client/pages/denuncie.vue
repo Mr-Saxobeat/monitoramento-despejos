84cc35363611e3eebdde2ed4a70f2ad08d211026
Weiglas <weiglasribeiro@gmail.com>
2021-05-10 15:56:43 -0300

    Cria formulário de denúncia

--- client/pages/denuncie.vue
+++ client/pages/denuncie.vue
@@ -1,93 +1,234 @@
 <template>
   <div class="container mt-4 mb-5">
       <h1>Denunciar despejo:</h1>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+    <b-form @submit="onSubmit">
       <b-form-group
         id="input-group-1"
-        label="Cidade:"
-        label-for="input-1"
+        label="Em qual município está ocorrendo a ação ou ameaças de despejo?"
+        label-for="input_cidade"
         description=""
       >
         <b-form-select
-            id="input-cidade"
+            id="input_cidade"
             type="select"
             placeholder="Selecione a cidade"
-            v-model="cidade_selecionada" 
+            v-model="form.cidade" 
             :options="cidades"
             required
         ></b-form-select>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+      <b-form-group 
+        id="input-group-2" 
+        label="Qual o nome da ocupação/comunidade onde ocorreu a ação ou ameaça de despejo?" 
+        label-for="input_nome_ocupacao">
         <b-form-input
-          id="input-2"
-          v-model="form.name"
-          placeholder="Enter name"
+          id="input-nome-da-ocupacao"
+          v-model="form.nome_ocupacao"
           required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.food"
-          :options="foods"
+      <b-form-group 
+        id="input-group-3" 
+        label="Quantas famílias estão sendo despejadas ou ameaçadas?" 
+        label-for="input_num_familias">
+        <b-form-input
+          type="number"
+          id="input_num_familias"
+          v-model="form.num_familias"
+          min=0
+          value=0
           required
-        ></b-form-select>
+        ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          v-model="form.checked"
-          id="checkboxes-4"
-          :aria-describedby="ariaDescribedby"
+      <b-form-group 
+        id="input-group-4" 
+        label="Qual o endereço ou ponto de referência da ocupação ou comunidade que está sofrendo o despejo / ameaça de despejo? (especificar da forma mais completa possível) " 
+        label-for="input_endereço">
+        <b-form-input
+          type="text"
+          id="input_endereco"
+          v-model="form.endereco"
+          required
         >
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
+        </b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-form-group 
+        id="input-group-5" 
+        label="Estágio do conflito:" 
+        label-for="input_situacao">
+        <b-form-select
+          id="input_situacao"
+          v-model="form.situacao"
+          required
+          :options="['despejo já ocorreu', 'ameaça sem previsão de data']"
+        >
+        </b-form-select>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-6" 
+        label="A ação ou ameaça de despejo ocorreu, está agendada ou tem grande risco de ocorrer durante a pandemia de Covid-19? (Março - período atual)" 
+        label-for="input_pandemia">
+        <b-form-select
+          id="input_pandemia"
+          v-model="form.pandemia"
+          required
+          :options="['sim', 'não']"
+        >
+        </b-form-select>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-7" 
+        label="Descreva o que aconteceu ou está acontecendo:" 
+        label-for="input_descricao">
+        <b-form-input
+          type="text"
+          id="input_descricao"
+          v-model="form.descricao"
+          required
+        >
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-8" 
+        label="Desde quando a comunidade/ocupação existe no local?"
+        description="Caso não tenha uma data exata, o campo pode ser preenchido com o dia 1º do respectivo mês e ano."
+        label-for="input_data_existencia">
+        <b-form-datepicker
+          id="input_data_existencia"
+          v-model="form.data_existencia"
+          required
+        >
+        </b-form-datepicker>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-9" 
+        label="Desde quando existe a ameaça de despejo?"
+        description="Caso não tenha uma data exata, o campo pode ser preenchido com o dia 1º do respectivo mês e ano."
+        label-for="input_data_ameaca_despejo">
+        <b-form-datepicker
+          id="input_data_ameaca_despejo"
+          v-model="form.data_ameaca_despejo"
+          required
+        >
+        </b-form-datepicker>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-10" 
+        label="Quando ocorreu ou para quando está agendado o despejo?"
+        description="Caso não tenha uma data exata, o campo pode ser preenchido com o dia 1º do respectivo mês e ano."
+        label-for="input_data_para_despejo">
+        <b-form-datepicker
+          id="input_data_para_despejo"
+          v-model="form.data_para_despejo"
+          required
+        >
+        </b-form-datepicker>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-11" 
+        label="Qual o caráter do imóvel?"
+        label-for="input_carater_imovel">
+        <b-form-select
+          id="input_carater_imovel"
+          v-model="form.carater_imovel"
+          :options="['público', 'privado']"
+          required
+        >
+        </b-form-select>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-12" 
+        label="Qual a tipologia da comunidade/ocupação? "
+        label-for="input_tipologia">
+        <b-form-select
+          id="input_tipologia"
+          v-model="form.tipologia"
+          :options="['ocupação de terreno', 'ocupação de edifício']"
+          required
+        >
+        </b-form-select>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-13"
+        label="Quem é responsável pela ameaça ou ação de despejo?"
+        label-for="input_responsavel_despejo">
+        <b-form-select
+          id="input_responsavel_despejo"
+          v-model="form.responsavel_despejo"
+          :options="[
+            'Proprietário privado', 
+            'Município',
+            'Governo do Estado',
+            'Governo Federal',
+            'Imobiliária',
+            'Ministério Público',
+            ]"
+          required
+        >
+        </b-form-select>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Enviar denúncia</b-button>
     </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
   </div>
 </template>
 
 <script>
   export default {
+    async asyncData({ $axios, params }) {
+      try{
+        let json_cidades = await $axios.$get('cidade/')
+        let cidades = []
+
+        json_cidades.features.forEach(cidade => {
+          cidades.push(cidade.properties.nome)
+        });
+
+        return { cidades }
+      } catch(e) {
+        console.log(e)
+        return []
+      }
+    },
     data() {
       return {
         form: {
-          email: '',
-          name: '',
-          food: null,
-          checked: []
+          cidade: null,
+          nome_ocupacao: null,
+          num_familias: null,
+          endereco: null,
+          situacao: null,
+          pandemia: null,
+          descricao: null,
+          data_existencia: null,
+          data_ameaca_despejo: null,
+          data_para_despejo: null,
+          carater_imovel: null,
+          tipologia: null,
+          responsavel_despejo: null
         },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
       }
     },
     methods: {
       onSubmit(event) {
         event.preventDefault()
-        alert(JSON.stringify(this.form))
+        let response = this.$axios.post('despejo/', data=this.form)
+        console.log(response)
       },
-      onReset(event) {
-        event.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
+    },
+    computed: {
     }
   }
 </script>
