9775ef5207f4b2c2d311ff5710282b93a2174cee
Weiglas <weiglasribeiro@gmail.com>
2021-05-17 19:49:47 -0300

    Ajusta navbar como bootstrapvue componente

--- client/components/Nav.vue
+++ client/components/Nav.vue
@@ -1,36 +1,60 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
-            <ul class="navbar-nav">
-                <NuxtLink class="navbar-brand" to="/">
-                    <b-img :src="require('@/assets/img/logo.png')" fluid class="logo"></b-img>
-                    Monitoramento de despejos
-                </NuxtLink>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  <b-navbar toggleable="lg" type="light">
+    <b-navbar-brand>
+        <NuxtLink class="navbar-brand" to="/">
+            <b-img class="navbar-brand-image" :src="require('@/assets/img/logo.png')" fluid ></b-img>
+            <span class="navbar-brand-text">
+                <span>
+                    Monitoramento de despejos<br>
+                    <small>Espírito Santo</small>
+                </span>
+            </span>
+        </NuxtLink>
+    </b-navbar-brand>
 
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <NuxtLink to="/" class="nav-item active">
-                        <span class="nav-link">Início</span>
-                    </NuxtLink>
-                    <NuxtLink to="/quem-somos/" class="nav-item">
-                        <span class="nav-link">Quem somos</span>
-                    </NuxtLink>
-                    <NuxtLink to="/mapeamento/" class="nav-item">
-                        <span class="nav-link">Mapeamento</span>
-                    </NuxtLink>
-                    <NuxtLink to="/publicacoes/" class="nav-item">
-                        <span class="nav-link">Publicações</span>
-                    </NuxtLink>
-                    <NuxtLink to="/denuncie/" class="nav-item">
-                        <span class="nav-link">Denuncie</span>
-                    </NuxtLink>
-                    <NuxtLink to="/contato/" class="nav-item">
-                        <span class="nav-link">Contato</span>
-                    </NuxtLink>
-                </div>
-            </ul>
-    </nav>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item>
+            <NuxtLink to="/" class="nav-item active">
+                <span class="nav-link">Início</span>
+            </NuxtLink>
+        </b-nav-item>
+
+        <b-nav-item>
+            <NuxtLink to="/quem-somos/" class="nav-item">
+                <span class="nav-link">Quem somos</span>
+            </NuxtLink>
+        </b-nav-item>
+
+        <b-nav-item>
+            <NuxtLink to="/mapeamento/" class="nav-item">
+                <span class="nav-link">Mapeamento</span>
+            </NuxtLink>
+        </b-nav-item>
+
+        <b-nav-item>
+            <NuxtLink to="/publicacoes/" class="nav-item">
+                <span class="nav-link">Publicações</span>
+            </NuxtLink>
+        </b-nav-item>
+
+        <b-nav-item>
+            <NuxtLink to="/denuncie/" class="nav-item">
+                <span class="nav-link">Denuncie</span>
+            </NuxtLink>
+        </b-nav-item>
+
+        <b-nav-item>
+            <NuxtLink to="/contato/" class="nav-item">
+                <span class="nav-link">Contato</span>
+            </NuxtLink>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
@@ -40,13 +64,23 @@
 </script>
 
 <style scoped>
-nav {
-    padding: 0;
-    margin: auto;
+.navbar{
+    padding-top: 0;
+    padding-bottom: 0;
     background-color: rgba(211, 210, 210, 0.46);
+    box-shadow: 0px 5px 15px black;
+    margin-bottom: 5px;
 }
-.logo {
-    width: 100px;
-    height: 100px;
+
+.navbar-brand, .navbar-brand-image{
+    padding-top: 0;
+    padding-bottom: 0;
+    height: 100%;
+}
+
+.navbar-brand-text {
+    display: inline-block;
+    text-align: center;
+    vertical-align: middle;
 }
 </style>
