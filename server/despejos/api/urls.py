9c0e6151651920d2b416cc7da8628fed503a65b0
Weiglas <weiglasribeiro@gmail.com>
2021-05-20 20:31:34 -0300

    Utiliza router para Despejo

--- server/despejos/api/urls.py
+++ server/despejos/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from despejos.api import views as v
+from rest_framework.routers import DefaultRouter
 
 
-urlpatterns = [
-    path('despejo/', v.DespejoAPIView.as_view(), name='despejo-list'),
+router = DefaultRouter()
+router.register(r'despejo', v.DespejoViewSet, basename='despejo')
+urlpatterns = router.urls
+
+urlpatterns += [
     path('cidade/', v.CidadeListView.as_view(), name='cidade-list'),
     path('layerdefinition/', v.LayerDefinitionView.as_view(), name='layer-definition-list'),
 ]
