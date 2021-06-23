f0aec737d14f007d79a6deaed863f5381111eae5
Weiglas <weiglasribeiro@gmail.com>
2021-06-23 18:23:37 -0300

    Ajusta python decouple

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v*da*z$_&5h8+*ja070#-+)07uf=^^r&wi6d(q+d0zm&p=9t7('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
