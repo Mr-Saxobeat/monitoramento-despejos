51a426129555c33c9082f6a166d654054d0da280
Weiglas <weiglasribeiro@gmail.com>
2021-07-11 15:23:17 -0300

    Add descricao Quem Somos

--- client/pages/quem-somos.vue
+++ client/pages/quem-somos.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="container mt-5">
-        <p>Monitoramento dos despejos no ES, coordenado pelo Celula Emau, realizado no escopo da Campanha Despejo Zero ES.</p>
+        <h2>NUDAM (Núcleo de Defesa Agrária e Moradia)</h2>
+        <p>O Núcleo de Defesa Agrária e Moradia (NUDAM), foi criado pelo ato normativo nº 450, de 15 de junho de 2015 e tem como função primordial a orientação jurídica, a promoção do direito à moradia e a regularização de áreas urbanas e rurais nos Municípios do Estado do Espírito Santo.<br>
+        Equipe: Vinícius Lamego de Paula</p>
+
+        <br>
+        <br>
+        <h2>CÉLULA EMAU</h2>
+        <p>Escritório Modelo de Arquitetura e Urbanismo da Universidade Federal do Espírito Santo<br>
+        LUTERO PROSCHOLDT ALMEIDA (coordenador), CLARA LUIZA MIRANDA (subcoordenador)</p>
     </div>
 </template>
 
