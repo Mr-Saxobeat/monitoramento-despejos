48bfe68f50afb1a757a0c265a583d110d9e59dba
Weiglas <weiglasribeiro@gmail.com>
2021-07-11 17:51:57 -0300

    Remove tag strong

--- client/components/Detalhes.vue
+++ client/components/Detalhes.vue
@@ -10,14 +10,14 @@
                 <b-card-text class="detalhes-content">
                     <div v-if="despejo">
                         <ul>
-                            <li><strong>Cidade:</strong> {{ despejo.properties.cidade }}</li>
-                            <li><strong>Ocupação:</strong> {{ despejo.properties.nome_ocupacao }}</li>
-                            <li><strong>Número de famílias:</strong> {{ despejo.properties.num_familias }}</li>
-                            <li><strong>Endereço:</strong> {{ despejo.properties.endereco }}</li>
-                            <li><strong>Descrição:</strong> {{ despejo.properties.descricao }}</li>
-                            <li v-if="data_existencia.ano > 1"><strong>Data de existência:</strong> {{ data_existencia.data }}</li>
-                            <li v-if="data_ameaca_despejo.ano > 1"><strong>Data de ameaça de despejo:</strong> {{ data_ameaca_despejo.data }}</li>
-                            <li v-if="data_para_despejo.ano > 1"><strong>Data marcada para o despejo:</strong> {{ data_para_despejo.data }}</li>
+                            <li>Cidade: {{ despejo.properties.cidade }}</li>
+                            <li>Ocupação: {{ despejo.properties.nome_ocupacao }}</li>
+                            <li>Número de famílias: {{ despejo.properties.num_familias }}</li>
+                            <li>Endereço: {{ despejo.properties.endereco }}</li>
+                            <li>Descrição: {{ despejo.properties.descricao }}</li>
+                            <li v-if="data_existencia.ano > 1">Data de existência: {{ data_existencia.data }}</li>
+                            <li v-if="data_ameaca_despejo.ano > 1">Data de ameaça de despejo: {{ data_ameaca_despejo.data }}</li>
+                            <li v-if="data_para_despejo.ano > 1">Data marcada para o despejo: {{ data_para_despejo.data }}</li>
                         </ul>
                     </div>
                     <div v-else>
@@ -70,12 +70,15 @@ export default {
 
 <style scoped>
 .detalhes-content{
-    height: 200px;
+    padding: 0;
+    height: 230px;
     overflow-x: hidden;
     overflow-y: auto;
 }
+
 ul {
     text-align: left;
     padding-left: 20px;
+    list-style: none;
 }
 </style>
